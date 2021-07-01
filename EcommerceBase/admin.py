2bc0742a6b380fcb11aee4418a6b8dc32ac11694
HAJIB Diae <diae.hajib@gmail.com>
2021-07-01 13:18:55 -0700

    Add files via upload

---
+++ EcommerceBase/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Privileges)
+
+admin.site.register(Admins)
+
+admin.site.register(Label)
+
+admin.site.register(Product)
+# Register your models here.
---
+++ EcommerceBase/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EcommercebaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'EcommerceBase'
